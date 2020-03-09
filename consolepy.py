baff4da359beabba61b3caebca5c96d940cfc99e
Viktor Patmalnieks <56944714+hamolicious@users.noreply.github.com>
2020-03-09 22:49:36 +0000

    major rewrite [backup]

--- consolepy.py
+++ consolepy.py
@@ -1,127 +1,187 @@
-"""
-XTERM CheatSheet : https://jonasjacek.github.io/colors/
-
-
-
-"""
-
-
 
 from os import system
 from math import sqrt, pi, sin, cos
+from random import randint
 
-class consolepy_error(Exception):
-    """ Base class for all errors """
-    pass
+## INITIALISATION FUNCTIONS
+def get_colors():
+    """ loads the colors """
+    with open('colors.txt', 'r') as file:
+        return file.readlines()
 
-class NeedToInit(consolepy_error):
-    """ Raised when the user has not initialised the module """
-    pass
+## CONTROLL FUNCTIONS
+def update():
+    global SCREEN, SCREEN_W, SCREEN_H
+    """ clears and draws the screen """
+    
+    to_draw = ''
+    for y in range(SCREEN_H):
+        to_draw += '\n'
+        for x in range(SCREEN_W):
+            try:
+                to_draw += SCREEN[f'{x}:{y}']
+            except KeyError:
+                to_draw += rgb_to_xterm(FILL)
 
-def color(xterm, char):
-    """ Formats the color to allow easier changes """
-    return f'\u001b[48;5;{xterm}m' + str(char) + '\u001b[0m'
+    system('cls')
+    print(to_draw)
+    SCREEN = {}
 
-def dist(x1, y1, x2, y2):
-    return sqrt((x2 - x1)**2 + (y2 - y1)**2)
+## ASSISTIVE FUNCTIONS
+def rgb_to_xterm(rgb):
+    global COLORS
+    """ return closest xterm version of rgb """
+    r1, g1, b1 = rgb
+    xterm = 0
 
-class consolepy():
-    #### SETUP FUNCTIONS
-    def init(self):
-        """ Initialises the module """
-        system('color')
-        self.fill = color(0, ' ')
-        self.to_render = {}
+    lowest = 100**10
 
-    def size(self, width, height):
-        """ sets the size for the screen """
-        self.width = width
-        self.height = height
+    for i in range(len(COLORS)):
+        xterm_check = int(COLORS[i].split(' ')[0])
+        r2 = int(COLORS[i].split(' ')[1])
+        g2 = int(COLORS[i].split(' ')[2])
+        b2 = int(COLORS[i].split(' ')[3])
 
-    def set_fill(self, char, xterm):
-        """ chooses the color and char to fill background with """
-        self.fill = color(xterm, char)
-
-    #### CONTROLL FUNCTIONS
-    def update(self):
-        """ updates the screen and draws it to the console """
-        screen = ''
+        if (dist := dist(r1, g1, b1, r2, g2, b2)) < lowest:
+            lowest = dist
+            xterm = xterm_check
         
-        for i in range(self.height):
-            screen += '\n'
-            for j in range(self.width):
-                if f'{j}:{i}' in self.to_render:
-                    screen += self.to_render[f"{j}:{i}"]
-                else:
-                    screen += self.fill
+    return f'\u001b[48;5;{xterm}m  \u001b[0m'
 
-        print(screen)
-        self.to_render = {}
-
-    def clear(self):
-        """ Clears console on Linux and Windows """
-        system('cls || clear')
-
-    #### DRAWING FUNCTIONS
-    def draw_point(self, x, y, char, xterm):
-        """ Draws a single character to screen """
-        self.to_render[f"{int(x)}:{int(y)}"] = color(xterm, char)
-
-    def draw_line(self, x1, y1, x2, y2, char, xterm):
-        """ draws a line from a to b """
-        # changes in x and y
-        dx = x2 - x1
-        dy = y2 - y1
-
-        # magnitude
-        mag = sqrt(dx**2 + dy**2)
-
-        # normalise magnitude
-        if mag == 0:
-            return
+def dist(*args):
+    if len(args) == 4:
+        x1, y1, x2, y2 = args
+        return sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    elif len(args) == 6:
+        x1, y1, z1, x2, y2, z2 = args
+    return sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
         
-        dx /= mag
-        dy /= mag
+## SETTINGS FUNCTIONS
+def set_fill(*args):
+    global FILL
+    """ sets the background fill """
 
-        while dist(x1, y1, x2, y2) > 1:#int(x1) != x2 and int(y1) != y2:
-            x1 += dx
-            y1 += dy
-            
-            self.to_render[f"{int(x1)}:{int(y1)}"] = color(xterm, char)
-            
-    def draw_rect(self, x, y, w, h, char, xterm, fill=False):
-        """ draws rectange at x y with a size of w h """
-        self.to_render[f"{int(x)}:{int(y)}"] = color(xterm, char)
-        self.to_render[f"{int(x + w)}:{int(y)}"] = color(xterm, char)
-        self.to_render[f"{int(x)}:{int(y + h)}"] = color(xterm, char)
-        self.to_render[f"{int(x + w)}:{int(y + h)}"] = color(xterm, char)
+    if len(args) == 3:
+        r, g, b = args
+        FILL = (r, g, b)
+    elif len(args) == 1:
+        col = args
+        FILL = (col, col, col)
 
-        if not fill:
-            self.draw_line(x, y, x + w, y, char, xterm)
-            self.draw_line(x, y + h, x + w, y + h, char, xterm)
-            self.draw_line(x + w, y, x + w, y + h, char, xterm)        
-            self.draw_line(x, y, x, y + h, char, xterm)
-        else:
-            for i in range(h+1):
-                for j in range(w+1):
-                    self.to_render[f"{int(j + x)}:{int(i + y)}"] = color(xterm, char)
+## DRAWING FUNCTIONS
+def draw_point(*args):
+    global SCREEN
+    """ draws a single point at xy """
 
-    def draw_circle(self, x, y, r, char, xterm, fill=False):
-        """ draws a circle at x y with a radius r """
-        a = pi
-        while a < pi*3:
-            cx = r * cos(a)
-            cy = r * sin(a)
-            a += 0.01
+    if len(args) == 5:
+        x, y, r, g, b = args
+    elif len(args) == 4:
+        x, y = args[0].get_xy()
+        r, g, b = args[1 : 4]
+    
+    col = rgb_to_xterm((r, g, b))
 
-            self.to_render[f"{int(x + cx)}:{int(y + cy)}"] = color(xterm, char)
+    SCREEN[f"{x}:{y}"] = col
 
-            if fill:
-                for i in range(r, 0, -1):
-                    fx = i * cos(a)
-                    fy = i * sin(a)
-                    
-                    self.to_render[f"{int(x + fx)}:{int(y + fy)}"] = color(xterm, char)
+def draw_line(*args):
+    global SCREEN
+    """ draws a line between x1y1 and x2y2 """
+
+    if len(args) == 7:
+        x1, y1, x2, y2, r, g, b = args
+    elif len(args) == 5:
+        x1, y1 = args[0].get_xy()
+        x2, y2 = args[1].get_xy()
+        r, g, b = args[2 : 5]
+
+    point_to_point_dist = lambda : dist(x1, y1, x2, y2)
+
+    col = rgb_to_xterm((r, g, b))
+
+    dx = x2 - x1
+    dy = y2 - y1
+
+    dist = point_to_point_dist()
+
+    dx /= dist
+    dy /= dist
+
+    while point_to_point_dist() > 1:
+        SCREEN[f"{int(x1)}:{int(y1)}"] = col
+
+        x1 += dx
+        y1 += dy
+
+## DATA STRUCTURES
+
+class Vector():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def random2D():
+        xdir = randint(-100, 100)
+        ydir = randint(-100, 100)
+
+        dist = dist(0, 0, xdir, ydir)
+
+        xdir /= dist
+        ydir /= dist
+
+        return Vector(xdir, ydir)
+
+    def __str__(self):
+        return f'X: {self.x}  |  Y: {self.y}'
+
+    def get_xy(self):
+        return (int(self.x), int(self.y))
+
+    def add(self, vec):
+        self.x += vec.x
+        self.y += vec.y
+
+    def sub(self, vec)
+        self.x -= vec.x
+        self.y -= vec.y
+
+# initialise module
+system('color')
+
+global FILL, SCREEN, SCREEN_W, SCREEN_H, COLORS
+COLORS = get_colors()
+FILL = (0, 0, 0)
+SCREEN = {}
+SCREEN_W, SCREEN_H = 30, 30
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
